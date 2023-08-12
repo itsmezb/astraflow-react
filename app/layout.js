7d2fa868b6bc4844d9ce5db0a158c6c30fbc4ed5
Sensei ZB <zaidahsd9991@gmail.com>
2023-08-12 23:28:13 +0200

    Update

--- app/layout.js
+++ app/layout.js
@@ -9,9 +9,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body>
-        <DevLinkProvider>
         {children}
-        </DevLinkProvider>        
         </body>
     </html>
   )
