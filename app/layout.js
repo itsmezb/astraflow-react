5b66830ca045fcd4f58f62648c1f108fd9a928e1
Sensei ZB <zaidahsd9991@gmail.com>
2023-08-12 22:27:32 +0200

    interactions

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,6 @@
 import '@/devlink/./global.css'
 import { Inter } from 'next/font/google'
-import "@/devlink/global.css";
+import { DevLinkProvider } from '@/devlink';
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -8,7 +8,11 @@ const inter = Inter({ subsets: ['latin'] })
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        <DevLinkProvider>
+        {children}
+        </DevLinkProvider>        
+        </body>
     </html>
   )
 }
